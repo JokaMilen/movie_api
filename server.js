d15ca929f6caf4f22d61fbed31b73091ed20d585
JokaMilen <77240807+JokaMilen@users.noreply.github.com>
2021-04-13 22:47:24 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require("http"),
+  url = require("url"),
+  fs = require("fs");
+
+http
+  .createServer((request, response) => {
+    let addr = request.url;
+    let q = url.parse(addr, true);
+    let filePath = "";
+    console.log("path: " + q.pathname);
+
+    if (q.pathname.includes("documentation")) {
+      filePath = "documentation.html";
+    } else {
+      filePath = "index.html";
+    }
+
+    fs.appendFile(
+      "log.txt",
+      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+      err => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log.");
+        }
+      }
+    );
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(data);
+      response.end();
+    });
+  })
+  .listen(8080);
+
+console.log("My first Node test server is running on Port 8080.");
