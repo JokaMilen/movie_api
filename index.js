72c184083f7d5b4eb4379383282c1c358023a2a1
JokaMilen <77240807+JokaMilen@users.noreply.github.com>
2021-04-27 12:35:39 +0200

    Create the Express code to route all of the endpoints, write down a list of URL endpoints and request methods

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-let topMovies = [
+let movies = [
   {
     title: "Mystic River",
     director: "Clint Eastwood"
@@ -40,14 +40,44 @@ let topMovies = [
   }
 ];
 
-// GET requests
+// GET all movies
 app.get("/movies", (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
 });
-
-app.get("/", (req, res) => {
-  asd.get();
-  res.send("Welcome to my favorite movies!");
+//Get the data about single movie
+app.get("/movies/:title", (req, res) => {
+  res.send(
+    "Successful GET request returning data on single movie by title: " +
+      req.params.title
+  );
+});
+//Get data about a genre
+app.get("/movies/genre/:name", (req, res) => {
+  res.send("Successful GET request returning data on genre by name");
+});
+//Get data about a director
+app.get("/movies/directors/:name", (req, res) => {
+  res.send("Successful GET request returning data about the director");
+});
+//Allow new users to register
+app.post("/users/:username", (req, res) => {
+  res.send("New user was registred");
+});
+//Allow users to update their user info
+app.put("/users/:userID", (req, res) => {
+  res.send("User info was updated");
+});
+//Allow users to add a movie to their list of favorites
+app.post("/users/:userID/favorites/:movieID", (req, res) => {
+  res.send("Movie was added to favorites");
+});
+//Allow users to remove a movie from their list of favorites
+app.delete("/users/:userID/favorites/:movieID", (req, res) => {
+  res.send("Movie was removed from favorites");
+});
+//Allow existing users to deregister
+app.delete("/users/:userID", (req, res) => {
+  res.send("User was removed");
 });
 
 app.get("/documentation", (req, res) => {
